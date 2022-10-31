4810463d5c7b1bc72cc9824d9a343790066d1602
Aleksandr Mangus <eterlate@gmail.com>
2022-10-31 12:54:27 +0600

    add items and mag search

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,19 +1,35 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useHttp } from '../hooks/http.hook'
 
 const MainPage = () => {
     const {loading, error, request} = useHttp()
     const [form, setForm] = useState({
-        mag_number: ''
+        mag_number: '',
+        doc_number: ''
     })
+
+    const [mags, setMags] = useState([])
+    const [items, setItems] = useState([])
+
     const changeHandler = event =>{
         setForm({...form, [event.target.name]: event.target.value})
     }
 
-    const magHandler = async () => {
+    useEffect( () => {
+
+    }, [error])
+
+    const formHandler = async () => {
         try{
-            const data = await request('/find/mag', 'POST', {...form})
-            console.log('data:', data)
+            if (form.doc_number == ''){
+                const data = await request('/find/mag', 'POST', {...form})
+                setMags(data.result.recordset)
+                setItems([])
+            }else{
+                const data = await request('/find/items', 'POST', {...form})
+                setItems(data.result.recordset)
+            }
+            
         }catch(e){}
     }
 
@@ -28,8 +44,34 @@ const MainPage = () => {
                 onChange={changeHandler}
                 placeholder='Введите номер магазина'
                 />
-                <button onClick={magHandler} disabled={loading}>Найти</button>
+
+                <label htmlFor="doc_number">Номер документа</label>
+                <input
+                type="text"
+                name='doc_number' 
+                onChange={changeHandler}
+                placeholder='Введите номер документа'
+                />
+                <button onClick={formHandler} disabled={loading}>Найти</button>
             </form>
+
+            {items.length > 0 ? 
+                <ul>
+                    {items.map(el =>
+                        <li key={el.itemName}>{el.itemName}</li>
+                    )}
+                </ul>
+            :
+                <ul>
+                    {mags.map(el =>
+                        <li key={el.docNumber}>{el.docNumber}</li>
+                    )}
+                </ul>  
+            }
+            
+            
+           
+            
             
         </div>
     )
--- routes/main_router.js
+++ routes/main_router.js
@@ -16,19 +16,31 @@ const {sendQuery} = require('../db')
 
 // /find
 router.post('/mag', async (req, res) =>{
-    
     try{
-        console.log(req.body)
         const {mag_number} = req.body
         
         if (!mag_number){
-            return res.status(400).json({message: 'enter number'})
+            return res.status(400).json({message: 'Empty number'})
         }
-        query = "SELECT * from marafettPrice where shopCode = \'" + mag_number + "\'"
+        query = "SELECT distinct docNumber from marafettPrice where shopCode = \'" + mag_number + "\' order by docNumber"
         result = await sendQuery(query)
-        console.log(result)
 
+        res.status(200).json({result})
+    }catch(e){
+        res.status(500).json({message: 'Server error, try again later'})
+    }
+})
 
+router.post('/items', async (req, res) =>{
+    try{
+        const {mag_number, doc_number} = req.body
+        if (!mag_number && !doc_number){
+            return res.status(400).json({message: 'Empty fields'})
+        }
+        query = "SELECT * from marafettPrice where shopCode = \'" + mag_number + "\' and docNumber = \'" + doc_number + "\'"
+        result = await sendQuery(query)
+
+        res.status(200).json({result})
     }catch(e){
         res.status(500).json({message: 'Server error, try again later'})
     }
