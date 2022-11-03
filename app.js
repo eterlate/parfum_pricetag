751bc33d5f05e2a90b9628fec2323777b1628ad9
Aleksandr Mangus <eterlate@gmail.com>
2022-11-03 15:06:14 +0600

    server

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const config = require('config')
 // const {sendQuery} = require('./db')
-
+const path = require('path')
 const app = express()
 
 app.use(express.json({ extended: true }))
@@ -9,7 +9,13 @@ app.use(express.json({ extended: true }))
 app.use(express.json({extended: true}))
 app.use('/find', require('./routes/main_router'))
 
+if(process.env.NODE_ENV === 'production'){
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
+    app.get('*', (req,res) =>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const PORT = config.get('server.port') || 5000
 
