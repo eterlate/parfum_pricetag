ce2b1d351f8bab8f6476f0d1bcbf2f3fd6558706
Aleksandr Mangus <eterlate@gmail.com>
2022-11-24 10:46:41 +0600

    big pricetag edit

--- client/src/components/BigPriceTag.jsx
+++ client/src/components/BigPriceTag.jsx
@@ -6,15 +6,35 @@ function BigPriceTag(props) {
             {props.item.percent != null ?
                 <div className='priceTagBig'>
                     <h1 id='itemNameBig'>{props.item.itemName}</h1>
-                    {props.item.percent < 10 ?
-                        <h1 id='percentBig'></h1>
-                        :
-                        <h1 id='percentBig'>скидка {props.item.percent}%</h1>
-                    }
-                    <h1 id='itemPrice1Big'>{props.item.itemPrice}</h1>
+                    <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                        {props.item.percent > 10 ?
+                            <>
+                                <div style={{ width: '200px' }}>
+                                    
+                                    <h1 style={{marginBottom:'10px'}} id='itemPrice1Big'>{props.item.itemPrice}</h1>
+                                    <h1 id='oldPriceHeaderBig'>старая цена</h1>
+                                    <h1 id='oldPriceBig'>{props.item.itemPriceOld}</h1>
+                                </div>
+                                <div style={{ width: '200px' }}>
+                                    <h1 id='percentBig'>-{props.item.percent}%</h1>
+                                </div>
+                            </>
+                            :
+                            <>
+                                <div style={{ width: '200px' }}>
+                                    <h1 id='oldPriceHeaderBig'>старая цена</h1>
+                                    <h1 style={{marginBottom:'-4px'}} id='oldPriceBig'>{props.item.itemPriceOld}</h1>
 
-                    <h1 id='oldPriceHeaderBig'>старая цена</h1>
-                    <h1 id='oldPriceBig'>{props.item.itemPriceOld}</h1>
+                                </div>
+                                <div style={{ width: '200px' }}>
+                                    <h1 id='itemPrice1Big'>{props.item.itemPrice}</h1>
+                                </div>
+                            </>
+
+
+                        }
+
+                    </div>
                     <div className='priceTagBottomBig'>
                         <h1 id='docDateBig'>{props.item.docDate}</h1>
                         <h1 id='itemCodeBig'>{props.item.itemCode}</h1>
@@ -22,10 +42,10 @@ function BigPriceTag(props) {
                 </div>
                 :
                 <div className='priceTagBig'>
-                    <div className='top'>
-                        <h1 id='itemNameBig'>{props.item.itemName}</h1>
-                        <h1 id='itemPriceBig'>{props.item.itemPrice}</h1>
-                    </div>
+
+                    <h1 id='itemNameBig'>{props.item.itemName}</h1>
+                    <h1 id='itemPriceBig'>{props.item.itemPrice}</h1>
+
                     <div className='priceTagBottomBig'>
                         <h1 id='docDateBig'>{props.item.docDate}</h1>
                         <h1 id='itemCodeBig'>{props.item.itemCode}</h1>
